935407d4ce7e833739ac0a2bbbb02c9ce93f3eb4
Suraj Vishwakarma <surajvish28556@gmail.com>
2020-03-09 17:27:38 +0530

    new program
    ok new program addedf

---
+++ java/src/demo/Main.java
@@ -0,0 +1,32 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		for(int j=0;j<n;j++){
+			ArrayList<ArrayList<Integer>> list=new ArrayList<ArrayList<Integer>>();
+			int i=scan.nextInt();
+			for(int z=0;z<i;z++){
+				int x=scan.nextInt();
+			}
+		}
+		double d = scan.nextDouble();
+		scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String s = scan.nextLine();
+		
+		
+	}
+
+}
+
+
