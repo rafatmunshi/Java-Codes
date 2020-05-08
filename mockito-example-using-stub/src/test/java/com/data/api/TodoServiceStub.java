c4e448250b1b494312239e2c80e60f4f23f2d3a5
surajvish28556 <surajvish28556@gmail.com>
2020-05-08 09:53:36 +0530

    mockito

--- mockito-example-using-stub/src/test/java/com/data/api/TodoServiceStub.java
+++ mockito-example-using-stub/src/test/java/com/data/api/TodoServiceStub.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 
 //This is stub class which gives you the data as dummy or it can act as a third party interface or the microservices which gives you data
+//The disadvantages of this class is you need to create the different methods for the different scenarios 
+//Or the different classes should be created for different microservices
 public class TodoServiceStub implements TodoService{
 
 	public List<String> retrieveTodos(String user) {
--- mockito-example-using-stub/src/test/java/com/demo/business/TodoBusinessImplInjectMock.java
+++ mockito-example-using-stub/src/test/java/com/demo/business/TodoBusinessImplInjectMock.java
@@ -1,8 +1,7 @@
 package com.demo.business;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.mockito.BDDMockito.given;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -10,22 +9,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import com.data.api.TodoService;
 import com.data.business.TodoBusinessImpl;
 
 //This class is to test the  the stub class which gives some data in return
-public class TodoBusinessImplMockTest {
+@RunWith(MockitoJUnitRunner.class) // This actually helps commenting the todoService mock refernce created.
+public class TodoBusinessImplInjectMock {
 
+	@Mock//this is basically writing code below.
+	TodoService todoServiceMock;
+	//TodoService todoServiceMock = mock(TodoService.class);
+	
+	@InjectMocks //this is basically writing code below.
+	TodoBusinessImpl todoBusinessImpl;
+	//TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+	
 	@Test
 	public void todoListRelatedToSpring() {
 		/*This mocking is used to create the virtual class of the interface which is the advantage of mocking*/
-		TodoService todoServiceMock = mock(TodoService.class);
+		//TodoService todoServiceMock = mock(TodoService.class);
 		List<String> todos=Arrays.asList("Learn Spring MVC", "Learn Spring","Learn to Dance");
 		/*after creating the class invoke the function of related class or interface and return the todos list as earlier 
 		our stub function is to give the list back in arraylist*/
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		//TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
 		List<String> todosList = todoBusinessImpl.retrieveTodoRelatedToSpring("Dummy");
 		assertEquals(2, todosList.size());   //Using assertequals gives you the value matching and returns you true value. 
 		//So create different test function test the various scenarios
@@ -33,29 +45,12 @@ public class TodoBusinessImplMockTest {
 	
 	@Test
 	public void todoListRelatedToSpring_empty() {
-		TodoService todoServiceMock = mock(TodoService.class);
+		//TodoService todoServiceMock = mock(TodoService.class);
 		List<String> todos=Arrays.asList();
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		//TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
 		List<String> todosList = todoBusinessImpl.retrieveTodoRelatedToSpring("Dummy");
 		assertEquals(0, todosList.size());   //Using assertequals gives you the value matching and returns you true value. So create different test function test the various scenarios
-	}
-	
-	@Test
-	public void todoListRelatedToSpring_usingBDD() {		
-		//GIVEN
-		TodoService todoServiceMock = mock(TodoService.class);
-		List<String> todos=Arrays.asList("Learn Spring MVC", "Learn Spring","Learn to Dance");
-		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
-		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		
-		//WHEN
-		List<String> todosList = todoBusinessImpl.retrieveTodoRelatedToSpring("Dummy");
-		
-		//THEN
-		//assertEquals(0, todosList.size());
-		assertThat(todosList.size(), is(2));    
-		//So create different test function test the various scenarios
 	}
 
 }
